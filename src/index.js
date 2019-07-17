dbdfc3d7f5cb4ce2fbefcb8c7b379cfe77db8477
Pavel Tryshankou <pavel.tryshankou@chorus.se>
2019-07-17 11:06:32 +0200

    Icons were added to the actions and few styles were updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEdit, faTrashAlt, faSearch } from '@fortawesome/free-solid-svg-icons';
 import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+
+library.add(faEdit);
+library.add(faTrashAlt);
+library.add(faSearch);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
